49319e04993e8d46ad2731ee3c78c3a6e13751fa
David Mar <david.bryan.mar@gmail.com>
2024-12-08 00:06:02 -0600

    Create appInfo.js

---
+++ appInfo.js
@@ -0,0 +1,7 @@
+exports.appInfo = {
+  appName: "SupertokensPasswordlessDemo",
+  apiDomain: "http://localhost:3001",
+  websiteDomain: "http://localhost:3000",
+  apiBasePath: "/auth",
+  websiteBasePath: "/auth"
+};
